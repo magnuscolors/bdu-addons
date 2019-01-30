5aae932622437a03abf2a570773b2c88cc2c2f94
Dave Prosee <>
2019-01-30 09:00:32 +0100

    BDU performance enhancement by removing slow queries

---
+++ bdu_performance_enhancements/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BDU performance enhancements",
+
+    'summary': """
+        Several interventions to improve user experience
+        """,
+
+    'description': """
+        
+    """,
+
+    'author': "D. Prosee",
+    'website': "http://www.bdu.nl",
+
+    'category': 'Uncategorized',
+    'version': '10.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/contacts.xml',
+    ],
+
+}
