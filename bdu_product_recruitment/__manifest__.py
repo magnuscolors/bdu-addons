ce509b13ac8590db7e2b63d6a305ff0a6329f8d9
Dave Prosee <>
2019-03-06 15:02:23 +0100

    bdu_product_recruitment and bdu_recruitment_online launch

---
+++ bdu_product_recruitment/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BDU product recruitment",
+
+    'summary': """
+        Order enhancements for product category Recruitment
+                """,
+
+    'description': """
+        Product Recruitment include:\n
+        - custom handling on orderline form\n
+        Usage:\n
+        - enhance products within custom handling \'Recruitment\' , found on product tab \'Sale\'\n
+        - or organize products under an enhanced product category with aforementioned custom handling \n
+        - the custom handling on the product (template) takes precedence over the one mentioned in the product category\n
+        - custom fields will show on orderline after selection of product
+    """,
+
+    'author'  : "D. Prosee",
+    'website' : "http://www.magnus.nl",
+    'license' : "LGPL-3", 
+    'category': 'sale',
+    'version' : '10.0',
+    'depends' : [
+                 'bdu_product_base', 
+                 'bdu_product_online', #list of websites
+                 'sale', 
+                ],
+    'data'    : [
+                'data/recruit_employment_type.xml',
+                'data/recruit_function_group.xml',
+                'data/recruit_education_level.xml',
+                'data/recruit_industry.xml',
+                 #'reports/appendix_distribution.xml',
+                 #'reports/list_for_distributors.xml',
+                 'views/orderline_adaptions.xml',
+                 #'views/distribution.xml',
+                 #'views/logistics_address_table_view.xml',
+                ],
+    'demo'    : [
+                ],
+}
