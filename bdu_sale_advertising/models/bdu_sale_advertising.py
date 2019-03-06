a4276d957b90cb6c27718f81cc1858107e60585e
Dave Prosee <>
2019-03-06 11:32:52 +0100

    bdu_sale_advertising : date keyword parsing for domain filter and invoice_date on scheduled action argument

--- bdu_sale_advertising/models/bdu_sale_advertising.py
+++ bdu_sale_advertising/models/bdu_sale_advertising.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import logging
+from datetime import date, timedelta
+import logging, pdb
 from odoo import api, fields, models, _
 
 _logger = logging.getLogger(__name__)
@@ -76,14 +77,22 @@ class SaleOrder(models.Model):
                     self.partner_acc_mgr = False
         return result
         
-    def invoice_filtered_order(self, domain):
-        # automated call expects domain selections in arguments in form of  (<arguments>,)
-        #example ([('state','=','sale'),('advertising','=',True)],)
+    def invoice_filtered_order(self, domain, invoice_date):
+        # automated call expects domain selections in arguments in form of  (<arguments>,invoice_date)
+        #example ([('state','=','sale'),('advertising','=',True)], "nearest_tuesday")
         if not type(domain) == list :
             _logger.error("Provided domain is not of type list. Program aborted.")
             return
+        helper=self.env['argument.helper']
+        for num, filter in enumerate(domain) :
+            lst         = list(filter)
+            lst[2]      = helper.date_parse(lst[2])
+            filter      = tuple(lst)
+            domain[num] = filter
+        invoice_date = helper.date_parse(invoice_date)
+        pdb.set_trace()
         selection = self.search(domain).ids
-        self = self.with_context(active_ids=selection,chunk_size=100)
+        self = self.with_context(active_ids=selection,chunk_size=100, invoice_date=invoice_date)
         result = self.env['ad.order.make.invoice'].make_invoices_from_ad_orders()
         return
 
@@ -98,14 +107,75 @@ class SaleOrderLine(models.Model):
         store=True
     )
         
-    def invoice_filtered_orderlines(self, domain):
-        # automated call expects domain selections in arguments in form of  (<arguments>,)
+    def invoice_filtered_orderlines(self, domain, invoice_date):
+        # automated call expects domain selections in arguments in form of  (<arguments>,invoice_date)
         #example ([('advertising','=',True),('state','=','sale'),('adv_issue.name', '=','ESO 2019-01-30')],)
         if not type(domain) == list :
             _logger.error("Provided domain is not of type list. Program aborted.")
             return
+        helper=self.env['argument.helper']
+        for num, filter in enumerate(domain) :
+            lst         = list(filter)
+            lst[2]      = helper.date_parse(lst[2])
+            filter      = tuple(lst)
+            domain[num] = filter
+        invoice_date = helper.date_parse(invoice_date)
         selection = self.search(domain).ids
-        self = self.with_context(active_ids=selection, chunk_size=100)
+        self = self.with_context(active_ids=selection, chunk_size=100, invoice_date=invoice_date)
         result = self.env['ad.order.line.make.invoice'].make_invoices_from_lines()
         return
+
+class ArgumentHelper(models.Model):
+    _name = 'argument.helper'
+
+    def date_parse(self, value) :
+        pdb.set_trace()
+        if value == 'first_this_month' :
+            now = date.today()
+            ftm = date(now.year, now.month, 1)
+            #move to monday if first day this month is a sunday
+            if ftm.weekday() == 6 :
+                ftm = date.date(now.year, now.month, 2)
+            return ftm.strftime('%Y-%m-%d')
+
+        elif value == 'last_day_previous_month' :
+            now  = date.today()
+            ldpm = date(now.year, now.month, 1)-timedelta(days=1)
+            #move to saturday is last day prev month is a sunday
+            if ldpm.weekday() == 5 :
+                ldpm = ldpm - timedelta(days=1)
+            return ldpm.strftime('%Y-%m-%d')
+
+        elif value == 'today' :
+            now = date.today()
+            return now.strftime('%Y-%m-%d')
+
+        elif value == 'yesterday' :
+            yesterday = date.today() - timedelta(days=1)
+            return yesterday.strftime('%Y-%m-%d')
+
+        elif value == 'day_before_yesterday' :
+            dby = date.today() - timedelta(days=2)
+            return dby.strftime('%Y-%m-%d')
+
+        elif value == 'last_sunday' :
+            now     = date.today()
+            weekday = now.weekday()
+            days    = -1 - weekday
+            ls      = now + timedelta(days=days)
+            return ls.strftime('%Y-%m-%d')
+
+        elif value == 'nearest_tuesday' :
+            now     = date.today()
+            weekday = now.weekday()
+            if weekday in [0,1,2,3,4] :
+                days = +1 -weekday
+            else :
+                days= +1 -weekdays +7  #saturday or sunday
+            nt = now + timedelta(days=days)
+            return nt.strftime('%Y-%m-%d')
+
+        else :
+            return value
+
 
